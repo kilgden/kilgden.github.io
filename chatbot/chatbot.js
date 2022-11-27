12878fc7437600ef5dd0b4cfa142140cf7d423b2
kilgden <72769603+kilgden@users.noreply.github.com>
2022-11-28 01:32:58 +0300

    7

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -399,3 +399,37 @@ SimpleChatbot.createTemplate = function() {
   document.body.appendChild($fragment);
   return document.querySelector('.chatbot');
 };
+// конфигурация чат-бота
+const configChatbot = {};
+// CSS-селектор кнопки, посредством которой будем вызывать окно диалога с чат-ботом
+configChatbot.btn = '.chatbot__btn';
+// ключ для хранения отпечатка браузера
+configChatbot.key = 'fingerprint';
+// реплики чат-бота
+configChatbot.replicas = { /*...*/ };
+// корневой элемент
+configChatbot.root = SimpleChatbot.createTemplate();
+// URL chatbot.php
+configChatbot.url = '/chatbot/chatbot.php';
+// создание SimpleChatbot
+let chatbot = new SimpleChatbot(configChatbot);
+// при клике по кнопке configChatbot.btn
+document.querySelector(configChatbot.btn).onclick = function (e) {
+  this.classList.add('d-none');
+  const $tooltip = this.querySelector('.chatbot__tooltip');
+  if ($tooltip) {
+    $tooltip.classList.add('d-none');
+  }
+  configChatbot.root.classList.toggle('chatbot_hidden');
+  chatbot.init();
+};
+// добавление ключа для хранения отпечатка браузера в LocalStorage
+let fingerprint = localStorage.getItem(configChatbot.key);
+if (!fingerprint) {
+  Fingerprint2.get(function (components) {
+    fingerprint = Fingerprint2.x64hash128(components.map(function (pair) {
+      return pair.value
+    }).join(), 31)
+    localStorage.setItem(configChatbot.key, fingerprint)
+  });
+}
